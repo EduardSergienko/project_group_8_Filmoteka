9e2710d3dfc31cc1cca4c34d1207fdb705894253
EduardSergienko <katleraster@gmail.com>
2022-06-12 14:36:00 +0300

    add cut

---
+++ src/JS/renderCards.js
@@ -0,0 +1,44 @@
+export function filmCardRender(arg, genresArrObj) {
+  return arg
+    .map(
+      item => ` <li class="films-list__card">
+      <img class = 'films-list__poster' src="https://image.tmdb.org/t/p/w500${
+        item.poster_path
+      }" alt=""  loading="lazy" />
+
+  <div class="film-info">
+  <h2 class="film-info__name">
+      ${cutFilmTitle(item.name || item.original_title)}  
+    </h2>
+    <div class="film-info__wrap">
+    <p class="film-info__genre-year">
+      ${getGenresNames(genresArrObj, item.genre_ids)}  |  ${(
+        item.first_air_date || item.release_date
+      ).slice(0, 4)} 
+      
+    </p>
+   
+    </div>
+  </div>
+</li>`
+    )
+    .join('');
+}
+export function getGenresNames(gengeArray, dataIds) {
+  const genresNames = gengeArray
+    .filter(genre => dataIds.includes(genre.id))
+    .map(genre => genre.name);
+
+  if (genresNames.length > 2) {
+    return genresNames.slice(0, 2).join(', ').concat([', Other']);
+  }
+  return genresNames.join(', ') || 'No Genre';
+}
+
+function cutFilmTitle(title) {
+  if (title.length > 40) {
+    return title.slice(0, 40) + '...';
+  } else {
+    return title;
+  }
+}
