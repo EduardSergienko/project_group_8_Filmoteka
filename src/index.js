736a1db96e2aedfe19dd891555c1558b8dabdbd3
BalVil <balutavili@gmail.com>
2022-06-13 18:08:27 +0300

    updated modal, change filmApi

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './JS/filmApiService';
 import './JS/showTrandingFilms';
-import './JS/btnWatchedQueue';
+// import './JS/btnWatchedQueue';
 import './JS/renderCards';
 import './JS/pagePagination';
+import './JS/modal';
