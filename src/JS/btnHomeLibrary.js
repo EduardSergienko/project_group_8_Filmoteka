d65583895b32fd50bdb2009970bc5d749a4100dc
Roman Maksimenko <romanmaksimenkodev@gmail.com>
2022-06-14 18:16:37 +0300

    header switch javascript

--- src/JS/btnHomeLibrary.js
+++ src/JS/btnHomeLibrary.js
@@ -1,9 +1,15 @@
 const homeNavBtn = document.querySelector('.pages__home-btn');
 const libraryNavBtn = document.querySelector('.pages__library-btn');
 const homeHeader = document.querySelector('.main-page__header');
-//const libraryHeader = document.querySelector('.header-library');
+const libraryHeader = document.querySelector('.header-library');
 libraryNavBtn.addEventListener('click', onLibraryNavBtnClick);
 function onLibraryNavBtnClick() {
   homeHeader.classList.toggle('is-hidden');
-  //libraryHeader.classList.toggle('is-hidden');
+  libraryHeader.classList.toggle('is-hidden');
+}
+const libraryHeaderLibbtn = document.querySelector('.header-library__button');
+libraryHeaderLibbtn.addEventListener('click', onLibraryHeaderBtnClick);
+function onLibraryHeaderBtnClick() {
+  homeHeader.classList.toggle('is-hidden');
+  libraryHeader.classList.toggle('is-hidden');
 }
