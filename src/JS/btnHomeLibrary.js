bb785a9097017344d976e7111a16c4deef75766c
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-06-15 13:23:18 +0300

    added home-page switch by clicking logo and library page home btn

--- src/JS/btnHomeLibrary.js
+++ src/JS/btnHomeLibrary.js
@@ -1,8 +1,15 @@
+import { showTranding } from './showTrandingFilms';
 const homeNavBtn = document.querySelector('.pages__home-btn');
 const libraryNavBtn = document.querySelector('.pages__library-btn');
 const homeHeader = document.querySelector('.main-page__header');
 const libraryHeader = document.querySelector('.header-library');
+const libraryHeaderHomebtn = document.querySelector(
+  '.header-library__home-button'
+);
+const headerLogo = document.querySelector('.header-lib-logo');
 libraryNavBtn.addEventListener('click', onLibraryNavBtnClick);
+libraryHeaderHomebtn.addEventListener('click', onLibraryHeaderHomeBtnClick);
+headerLogo.addEventListener('click', onHeaderLogoClick);
 
 function onLibraryNavBtnClick() {
   homeHeader.classList.toggle('is-hidden');
@@ -10,15 +17,18 @@ function onLibraryNavBtnClick() {
   libraryHeader.classList.toggle('is-hidden');
   //libraryNavBtn.classList.toggle('current');
 }
-const libraryHeaderHomebtn = document.querySelector(
-  '.header-library__home-button'
-);
-
-libraryHeaderHomebtn.addEventListener('click', onLibraryHeaderHomeBtnClick);
 
 function onLibraryHeaderHomeBtnClick() {
   homeHeader.classList.toggle('is-hidden');
   // homeNavBtn.classList.toggle('current');
   libraryHeader.classList.toggle('is-hidden');
   //libraryNavBtn.classList.toggle('current');
+  showTranding();
+}
+
+function onHeaderLogoClick() {
+  console.log('click');
+  libraryHeader.classList.add('is-hidden');
+  homeHeader.classList.remove('is-hidden');
+  showTranding();
 }
