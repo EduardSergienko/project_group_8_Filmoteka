46a8d49e0990b900043236b12acf3abdfdeb6d86
BalVil <balutavili@gmail.com>
2022-06-16 12:31:42 +0300

    add fetch images + styling modal

--- src/JS/filmApiService.js
+++ src/JS/filmApiService.js
@@ -32,6 +32,12 @@ export default class FilmApiService {
     return await axios.get(`${BASE_URL}${this.ID}?${API_KEY}&language=en-US`);
   }
 
+  async fetchMovieImages() {
+    const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+    const API_KEY = 'api_key=5f364d2fc6b25c805674b50a1c63d59e';
+    return await axios.get(`${BASE_URL}${this.ID}/images?${API_KEY}`);
+  }
+
   resetPage() {
     this.page = 1;
   }
