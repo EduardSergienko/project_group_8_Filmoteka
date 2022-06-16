4271c81de99727989ce3e074f355930117d520b7
Roman Maksimenko <romanmaksimenkodev@gmail.com>
2022-06-16 19:05:24 +0300

    dark theme

--- src/JS/toolbar.js
+++ src/JS/toolbar.js
@@ -8,6 +8,7 @@ const refs = {
   footer: document.querySelector('.footer'),
   footerLink: document.querySelector('.team__link'),
   scroll: document.querySelector('.scroll-process'),
+  container: document.querySelector('.container'),
 };
 
 const active = 'active-toolebar';
@@ -16,6 +17,7 @@ const NIGHT = 'Night';
 const BODY = 'body';
 const FOOTER = 'footer';
 const SKROLL = 'skroll';
+const CONTAINER = 'container';
 const COLORFOOTERLINK = 'colorFooterLink';
 
 refs.toolbarBtn.addEventListener('click', togle);
@@ -45,10 +47,12 @@ function nightTheme() {
   refs.footer.classList.add('dark-theme');
   refs.footerLink.classList.add('dark-theme');
   refs.scroll.classList.add('dark');
+  refs.container.classList.add('dark-theme');
   saveStorage(BODY, 'dark-theme');
   saveStorage(FOOTER, 'dark-theme');
   saveStorage(COLORFOOTERLINK, 'dark-theme');
   saveStorage(SKROLL, 'dark-theme');
+  saveStorage(CONTAINER, 'dark-theme');
 
   refs.sunSvg.classList.add(active);
   saveStorage(SUN, active);
@@ -62,12 +66,14 @@ function dayTheme() {
   removeStorage(FOOTER);
   removeStorage(COLORFOOTERLINK);
   removeStorage(SKROLL);
+  removeStorage(CONTAINER);
 
   refs.body.classList.remove('dark-theme');
   refs.footer.classList.remove('dark-theme');
   refs.footerLink.classList.remove('dark-theme');
   refs.sunSvg.classList.remove(active);
-  refs.scroll.classList.remove('dark-theme');
+  refs.scroll.classList.remove('dark');
+  refs.container.classList.remove('dark-theme');
 
   refs.moonSvg.classList.add(active);
   saveStorage(NIGHT, active);
